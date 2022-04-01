41e869573b38ad2e4e89622ea45096a1c8f00ab9
TyfangXV <9fangdev@gmail.com>
2022-04-01 23:46:42 +0400

    tweaked some ui changes

--- mobile/src/components/OnGoingTaskComponent.tsx
+++ mobile/src/components/OnGoingTaskComponent.tsx
@@ -1,5 +1,5 @@
 import { Ionicons } from '@expo/vector-icons';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import {
   View,
   Text,
@@ -7,17 +7,20 @@ import {
   Pressable,
   Touchable,
   TouchableOpacity,
+  useWindowDimensions,
 } from 'react-native';
 import { Divider } from 'react-native-elements';
 import { Task } from '../../types';
 
 const OnGoingTaskComponent: React.FC<Task> = ({ title, settings }) => {
+  const {height, width} = useWindowDimensions();
+  
   return (
     <View style={[style.container, { backgroundColor: '#B90085' }]}>
       <Text style={style.title}>
-        {title.split('').length > 12
-          ? title.split('').slice(0, 14).concat('...')
-          : title}
+        {
+          title.length > 15 ? `${title.substring(0, 15)}...` : title
+        }
       </Text>
       <View style={style.colum}>
         <Ionicons
@@ -27,7 +30,7 @@ const OnGoingTaskComponent: React.FC<Task> = ({ title, settings }) => {
           style={style.icons}
         />
         <Pressable style={style.btn}>
-          <Ionicons name="time" size={28} color="white" style={style.icons} />
+          <Ionicons name="time" size={28} color="white" style={[style.icons, {includeFontPadding : false}]} />
           <Text style={{color : "white", paddingLeft : 5, fontFamily : "Amiko-Bold"}}>Completing</Text>
         </Pressable>
       </View>
@@ -37,21 +40,22 @@ const OnGoingTaskComponent: React.FC<Task> = ({ title, settings }) => {
 
 const style = StyleSheet.create({
   container: {
-    padding: 5,
+    paddingTop : 10,
+    paddingBottom : 10,
+    padding : 5,
     flexDirection: 'column',
     justifyContent: 'space-between',
     margin: 5,
     maxWidth: '48%',
     minWidth: '48%',
     borderRadius: 5,
-    height: 100,
+    height: 115,
   },
   title: {
     fontSize: 20,
     fontFamily: 'Amiko-Bold',
     color: 'white',
-    textDecorationLine: 'underline',
-    textDecorationStyle: 'solid',
+    textAlignVertical : "center"
   },
   colum: {
     justifyContent: 'space-between',
